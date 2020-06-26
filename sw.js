3c2141a08875245f8ec4ea9e4e06f87ebfff2e72
ivanBalev <43599235+ivanBalev@users.noreply.github.com>
2020-06-26 09:59:03 +0300

    Implemented shell assets caching

--- sw.js
+++ sw.js
@@ -1,9 +1,34 @@
+const staticCacheName = 'site-static'
+const assets = [
+    '/',
+    '/index.html',
+    '/js/app.js',
+    '/js/ui.js',
+    '/js/materialize.min.js',
+    '/css/styles.css',
+    '/css/materialize.min.css',
+    'img/dish.png',
+    'https://fonts.googleapis.com/icon?family=Material+Icons'
+]
+
 // install service worker
 self.addEventListener('install', evt => {
-    console.log('service worker has been installed');
+    console.log('installing new service worker')
+    evt.waitUntil(
+        caches.open(staticCacheName)
+            .then((cache) => {
+                console.log('caching shell assets');
+                // .addAll() & .add() make requests so we store the urls for those requests in the assets array
+                cache.addAll(assets)
+            })
+    )
 })
 
 // activate service
-self.addEventListener('activate', (evt) => {
+self.addEventListener('activate', evt => {
     console.log('service worker has been activated');
+})
+
+// fetch events
+self.addEventListener('fetch', evt => {
 })
