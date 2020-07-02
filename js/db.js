5553fcfd41c63f951a17a1a11504821f6f7c26ce
ivanBalev <43599235+ivanBalev@users.noreply.github.com>
2020-07-02 23:25:20 +0300

    Offline data addition and use of IndexedDB implemented

---
+++ js/db.js
@@ -0,0 +1,45 @@
+// offline data
+db.enablePersistence()
+    .catch((err) => {
+        if (err.code === 'failed-precondition') {
+            // multiple tabs open at once
+            console.log('persistence failed');
+        } else if (err.code === 'unimplemented') {
+            // lack of browser support
+            console.log('persistence is not available');
+        }
+    })
+
+// real-time listener
+db.collection('recipes').onSnapshot((snapshot) => {
+    snapshot.docChanges().forEach(change => {
+        if (change.type === 'added') {
+            // add the document data to the web page
+            renderRecipe(change.doc.data(), change.doc.id);
+        }
+        if (change.type === 'removed') {
+            // remove the document data from the web page
+        }
+    })
+})
+
+
+//add new recipe
+const form = document.querySelector('form');
+form.addEventListener('submit', (evt) => {
+    evt.preventDefault();
+
+    const recipe = {
+        title: form.title.value,
+        ingredients: form.ingredients.value
+    }
+
+    db.collection('recipes').add(recipe)
+    .catch(err => {
+        console.log(err);
+    });
+
+    form.title.value ='';
+    form.ingredients.value ='';
+})
+
